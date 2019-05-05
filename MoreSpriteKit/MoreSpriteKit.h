4515776f5ec9546e543e196cde6420795bd56a8f
sander frenken <sander.frenken@hotmail.com>
2019-05-05 21:23:37 +0200

    initial commit

---
+++ MoreSpriteKit/MoreSpriteKit.h
@@ -0,0 +1,19 @@
+//
+//  MoreSpriteKit.h
+//  MoreSpriteKit
+//
+//  Created by sander frenken on 05/05/2019.
+//  Copyright © 2019 rednasGames. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MoreSpriteKit.
+FOUNDATION_EXPORT double MoreSpriteKitVersionNumber;
+
+//! Project version string for MoreSpriteKit.
+FOUNDATION_EXPORT const unsigned char MoreSpriteKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MoreSpriteKit/PublicHeader.h>
+
+
