3e40e714197d89d9d4ab599c805e596eb0cf9915
sander frenken <sander.frenken@hotmail.com>
2020-04-18 15:06:55 +0200

    Refactor gradient drawing procedure to avoid dangling pointer

--- MoreSpriteKit/SKTexture+RadialGradient.swift
+++ MoreSpriteKit/SKTexture+RadialGradient.swift
@@ -8,13 +8,17 @@ public extension SKTexture {
             let colorSpace = CGColorSpaceCreateDeviceRGB()
             let cgColors = colors.map({ $0.cgColor }) as CFArray
 
-            guard let gradient = CGGradient(colorsSpace: colorSpace, colors: cgColors, locations: UnsafePointer<CGFloat>(locations)) else {
+            var gradient: CGGradient?
+            locations.withUnsafeBufferPointer { locationBuffer in
+                gradient = CGGradient(colorsSpace: colorSpace, colors: cgColors as CFArray, locations: locationBuffer.baseAddress)
+            }
+            guard let gradientUnwrapped = gradient else {
                 fatalError("Failed creating gradient.")
             }
 
             let radius = max(size.width, size.height) / 2.0
             let midPoint = CGPoint(x: size.width / 2.0, y: size.height / 2.0)
-            context.cgContext.drawRadialGradient(gradient, startCenter: midPoint, startRadius: 0, endCenter: midPoint, endRadius: radius, options: [])
+            context.cgContext.drawRadialGradient(gradientUnwrapped, startCenter: midPoint, startRadius: 0, endCenter: midPoint, endRadius: radius, options: [])
         }
 
         self.init(image: image)
