b0fac620052bdd7137b4257d9828051fd7f830a4
Frenken, S.H. (Sander) <>
2024-10-18 11:44:10 +0200

    Fix Concurrency issue

--- Sources/MoreSpriteKit/SKAction+Spiral.swift
+++ Sources/MoreSpriteKit/SKAction+Spiral.swift
@@ -4,7 +4,7 @@ public extension SKAction {
 
     // Adapted from
     // https://stackoverflow.com/questions/30427482/moving-a-skspritenode-in-a-downward-loop-using-swift/38235468
-    static func spiral(startRadius: CGFloat, endRadius: CGFloat, totalAngle: CGFloat, centerPoint: CGPoint, duration: TimeInterval) -> SKAction {
+    @MainActor static func spiral(startRadius: CGFloat, endRadius: CGFloat, totalAngle: CGFloat, centerPoint: CGPoint, duration: TimeInterval) -> SKAction {
 
         let radiusPerRevolution = (endRadius - startRadius) / totalAngle
         let action = SKAction.customAction(withDuration: duration) { node, time in
